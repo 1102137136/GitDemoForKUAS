41e73a1534bd3ac4a114d5d7178e5b689d383c81
1102137136 <1102137136@gm.kuas.edu.tw>
2016-03-03 16:03:17 +0800

    add Pokemon

---
+++ WindowsFormsApplication1/Pokemon.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApplication1
+{
+    public class Pokemon
+    {
+        public static void Main1()
+        {
+            System.Console.WriteLine("apple");
+            System.Console.ReadLine();
+        }
+    }
+}            
